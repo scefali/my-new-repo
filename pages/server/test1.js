35966b9746749227349a7b0d9cb23578c10c990a
Stephen Cefali <stephencefali@gmail.com>
2020-07-29 15:24:49 -0700

    update

--- pages/server/test1.js
+++ pages/server/test1.js
@@ -1,7 +1,7 @@
 const Test1 = () => <h1>Server Test 1</h1>
 
 export function getServerSideProps() {
-  throw new Error('Server Test 1')
+  throw new Error("My new error")
 }
 
 export default Test1
