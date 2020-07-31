e55268c75dbd578e70d5d6045594ecca18c9e322
Stephen Cefali <scefali@sentry.io>
2020-07-31 15:26:48 -0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -18,3 +18,9 @@ if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   })
 }
+
+
+export default function App({ Component, pageProps, err }) {
+  // Workaround for https://github.com/vercel/next.js/issues/8592
+  return <Component {...pageProps} err={err} />
+}
